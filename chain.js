af38e2a87776446688347db0ed9f1111fb30b242
Sansthita <73059473+Sansthita@users.noreply.github.com>
2020-11-15 15:51:15 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,16 @@
+class Chain{
+    constructor(bodyA, bodyB){
+        var options={
+            length: 2.0,
+            stiffness: 0.04,
+            bodyA:bodyA,
+            bodyB:bodyB
+        }
+       
+        this.chain = Matter.Constraint.create(options);
+        World.add(world, this.chain);
+    }
+    display(){
+        line(this.chain.bodyA.position.x, this.chain.bodyA.position.y, this.chain.bodyB.position.x, this.chain.bodyB.position.y)
+    }
+}
